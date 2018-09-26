50a1fccedc439375e027e384a9e630d2050d918f
Tuomas Salo <tuomas.salo@iki.fi>
2018-09-26 17:32:52 +0300

    fix globals use

--- src/clicklinkcommand.js
+++ src/clicklinkcommand.js
@@ -1,15 +1,15 @@
 import Command from '@ckeditor/ckeditor5-core/src/command'
 import {showUI} from './utils'
 
-export default class UnlinkCommand extends Command {
+export default class ClicklinkCommand extends Command {
   refresh() {
     this.isEnabled = this.editor.model.document.selection.hasAttribute('richLink')
   }
 
   execute() {
-    const linkCommand = editor.commands.get('link')
+    const linkCommand = this.editor.commands.get('link')
     if (linkCommand.isEnabled) {
-      showUI(editor)
+      showUI(this.editor)
     }
   }
 }
